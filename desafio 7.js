6fc58caa8608246d21e1ed7d549865d4b1eabc02
JCPurger <jc.purger@gmail.com>
2023-02-27 22:34:57 -0300

    olha só, olha esses raios de luz

--- desafio 7.js
+++ desafio 7.js
@@ -1,9 +1,15 @@
 // [ ] Faça um programa onde leia um numero e diga se ele é par ou impar.
 
-const number = 1
+const number = 2
+const resto  = number % 2
 
-if ( number % 2 === 0) {
-    console.log("O número é par")
-} else {
+/**
+ *  true  -> 1
+ *  false -> 0
+ */
+
+if (resto) {
     console.log("O número é ímpar")
+} else {
+    console.log("O número é par")
 }
